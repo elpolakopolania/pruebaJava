a127dfe1999f699b5294c6b4ebd8a9fdc3853b85
elpolakopolania <gustav0796@hotmail.com>
2021-11-24 17:23:37 -0500

    Primer commit

---
+++ src/main/java/com/prueba/java/sprintboot/pruebaJava/PruebaJavaApplication.java
@@ -0,0 +1,13 @@
+package com.prueba.java.sprintboot.pruebaJava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PruebaJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PruebaJavaApplication.class, args);
+	}
+
+}
